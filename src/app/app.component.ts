e5b253ff7ea93bc0f4e62f1e2b292ca0f2340679
Sree Kariyadan <sree@Srees-MacBook-Pro.local>
2024-09-19 00:19:42 +0530

    uhm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'skariyadan.github.io';
+  title = 'Sree Kariyadan ┋ UX Designer';
 }
