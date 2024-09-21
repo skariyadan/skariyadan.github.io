911321e1491086090364b4fd3c92b272e837f0d3
Sree Kariyadan <sree@Srees-MacBook-Pro.local>
2024-09-21 20:19:49 +0530

    json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
+import {ElementRef, HostListener, ViewChild } from '@angular/core';
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,6 +10,29 @@ import { NavigationComponent } from '../navigation/navigation.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+
+export class HomeComponent{
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() 
+  {
+    {
+      console.log(pageYOffset);
+      const demoDiv = document.querySelector("#line-content");
+      if (demoDiv)
+      {
+        if(pageYOffset > 0)
+          {
+            demoDiv.classList.add("no-hover");
+          }
+          else
+          {
+            demoDiv.classList.remove("no-hover");
+          }
+        demoDiv.setAttribute('style', 'height: '+(pageYOffset+140)+'px;'); 
+        console.log(demoDiv.getAttribute('style'));
+      }
+    }
+  }
+ 
 
 }
